a5d017b1473a063ff1d697a396cadfd1e3651c32
M0rfes <fahimkhan20148@gmail.com>
2025-10-04 20:53:25 +0400

    Implement Express.js application with AsyncLocalStorage for logging

---
+++ index.ts
@@ -0,0 +1,46 @@
+import express from 'express'
+import { AsyncLocalStorage } from 'node:async_hooks'
+import { v4 as uuidv4 } from 'uuid'
+import winston from 'winston'
+
+type Context = {
+  logger: winston.Logger
+}
+
+const asyncLocalStorage = new AsyncLocalStorage<Map<keyof Context, Context[keyof Context]>>()
+
+// Winston logger setup
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+})
+
+// Middleware to store logger with unique request id in context
+function loggerMiddleware(req: express.Request, res: express.Response, next: express.NextFunction) {
+  const reqId = req.header('x-uuid') || uuidv4()
+  const reqLogger = logger.child({ reqId }) // Attach x-uuid to logger
+  asyncLocalStorage.run(new Map([['logger', reqLogger]]), () => next())
+}
+
+function getLogger() {
+  return asyncLocalStorage.getStore()?.get('logger') || logger
+}
+
+
+const app = express();
+
+app.use(loggerMiddleware);
+
+app.get('/', async (req: express.Request, res: express.Response) => {
+  getLogger().info('Request received', { action: 'start' });
+  // ... any async work here ...
+  res.send('hello world');
+});
+
+app.listen(3000, () => {
+  logger.info('Server is running on port 3000');
+});
